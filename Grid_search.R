0484dd120a9c8e2bff0fffb0976fd46f343f53b3
Veronica Malizia <veronica.malizia93@gmail.com>
2023-02-21 13:49:44 +0000

    Separate file for setting scenario

--- Grid_search.R
+++ Grid_search.R
@@ -22,6 +22,7 @@ library(doParallel)
 library(patchwork)
 library(profvis)
 library(rstudioapi)
+library(deSolve)
 
 ################
 #Initial population
@@ -89,64 +90,10 @@ I0=0
 S0=snail.pop - sum(E0, I0)
 C0=0
 
-################
-#Simulation settings
-################
-T <- 200 #number of years simulated
-seeds <- 10
-fr <- 10 #frequency for printing to file the individual output [years]
-write.output <- FALSE #disable individual output for grid search (saving time)
-
-################
-#SETTING THE MODELLING SCENARIO: grid search
-################
-#For each limiting mechanism, choose level: 'No', 'Mild', 'Strong'
-#Combinations of modelling scenarios and stochastic seed
-
-#No mda
-parms$mda$start <- 0
-parms$mda$end <- 0
-
-#Grid search of endemic parameters
-stoch_scenarios <- expand.grid(list(seed = 1:seeds,
-                                    DDF_strength = c("Strong"),
-                                    imm_strength = c("Absent"),
-                                    snails = c("Strong"),
-                                    # zeta = exp(runif(50, -9.9, -9.5)),
-                                    # worms_aggr = seq(0.1, 0.29, 0.01)))
-                                    zeta = runif(40, 10^(-3), 4*10^(-2)),
-                                    worms_aggr = seq(0.1, 0.3, 0.05),
-                                    tr_snails = c(10^(-6), 10^(-5), 10^(-4))))
-
-
-
-#transmission_snails <- seq(0.000001, 0.0001, 0.00001)
-
-#Load tuned transmission parameters (zetas) for the scenarios above (attention to the order!) 
-#Transmission parameters for tuning endemicity
-# No need in case of grid search
-# zetas <- read_excel("Zetas.xlsx")
-# stoch_scenarios <- mutate(stoch_scenarios, zeta = rep(zetas$Zeta, each = seeds))
-
-#Load matched alphas for Density-dependent fecundity (DDF) given the endemicity
-load("Matched_alphas_moderate.RData")
-
-#Heterogeneity of worms
-#parms$parasite$k_w = 0.3
-
-################
-#Set output directory to save results
-################
-setting <- "Grid_search_panel7_DDFstrong"
-  #"Moderate_setting_k=030_Age-int"
-
-#This will be the directory where the individual output is automatically saved throughout the simulations
-if(write.output == TRUE){
-  ind.output.dir <- file.path(source.dir, paste("Output/Individual/", setting, sep = "")) 
-  dir.create(ind.output.dir) #Add check: this command to be run only if the directory is not existent
-  #Empty the Output folder (only if needed)
-  #unlink(file.path(output.dir, "/*")) 
-}
+#################
+#Preparing simulation setting
+#################
+source("Setting_simulation_scenario.R")
 
 ################
 #Run the model
@@ -167,8 +114,8 @@ if(!file.exists(pop.output.dir)){
 }
 
 #Check indexes without errors (the snail system goes to zero)
-index <- which(sapply(results, length)>2)
-results <- results[index]
+# index <- which(sapply(results, length)>2)
+# results <- results[index]
 
 #Collating and saving population-level output
 #Individual output is automatically saved through the simulations
